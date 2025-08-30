d67a352df4f7ccb999eb7a00539f34539440a0a1
franlugea <franlugea03@gmail.com>
2025-08-30 17:18:21 -0300

    Arreglando errores

--- src/main/java/SistemasDistribuidos/Entity/ActividadFisica.java
+++ src/main/java/SistemasDistribuidos/Entity/ActividadFisica.java
@@ -18,7 +18,6 @@ public class ActividadFisica {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Enumerated(EnumType.STRING)
     private ActividadFisicaEnum nombre;
 
